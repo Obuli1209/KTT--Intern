ce115ce0037b2be385509b5305583785943303d4
Obuli1209 <obulimohanasundaram@gmail.com>
2025-04-01 18:19:29 +0530

    april_1

---
+++ JS_Notes/js_april1.js
@@ -0,0 +1,24 @@
+//object
+//object created by curly brackets with list of properties.
+//properties is a key:value pairs.
+//key property should be in string  it as propery name and value as any of them.
+//object create in two syntax
+
+
+//object constructor
+let user = new Object();
+
+
+//object literal
+let user1={};
+
+let user2 = {
+    name: "John",
+    age: 30,
+    sibling:{
+        brother:"abishek",
+        sister:"dhana",
+    }
+  }
+
+console.log(user2.sibling.brother);
